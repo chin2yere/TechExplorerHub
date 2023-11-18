29fd9ccf5a30129a9c477c1828b7f6fbad34a337
Chinyere offor <obiagelichinyere566@gmail.com>
2023-11-17 23:33:05 -0500

    deployment completedd

--- server/server.js
+++ server/server.js
@@ -32,6 +32,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server listening on http://localhost:${PORT}`);
 });

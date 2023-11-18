da9e205996e599b5b69561f0eb747d6be1c771ae
Chinyere offor <obiagelichinyere566@gmail.com>
2023-11-17 21:02:16 -0500

    step 0 deployment1

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
-import express from 'express'
-import './config/dotenv.js'
+import express from "express";
+import "./config/dotenv.js";
 import cors from "cors";
 import morgan from "morgan";
 
-import router from './config/routes.js'
+import router from "./config/routes.js";
 
-import setup from './Database/setup.js'
-setup()
+import setup from "./Database/setup.js";
+setup();
 
-const app = express()
+const app = express();
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -19,14 +19,14 @@ app.use(
 app.use(express.json()); // Middleware for parsing JSON bodies from HTTP requests
 app.use(morgan("tiny"));
 
-app.use('/', router)
+app.use("/", router);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Video Games API</h1>')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Video Games API</h1>");
+});
 
-const PORT = process.env.PORT || 3000
-    
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`🚀 Server listening on http://localhost:${PORT}`)
-})
+  console.log(`🚀 Server listening on http://localhost:${PORT}`);
+});
